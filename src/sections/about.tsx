3a48b8500129a9f91d7f19770243373ed864ee48
Shashank Ajmera <shashanksajmera@gmail.com>
2025-07-06 21:55:38 +0530

    checking images

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -20,7 +20,7 @@ export default function AboutUs() {
                 ref.current.style.maxHeight = "none"
             }
             else {
-                ref.current.style.maxHeight = "8.4rem"
+                ref.current.style.maxHeight = "9rem"
             }
         }
     }, [isExpanded])
@@ -32,7 +32,7 @@ export default function AboutUs() {
                     <MultilingualText
                         englishText={"Mahavir Diagnostic Center, run by Mahavir Eye Foundation in Chhatrapati Sambhajinagar, is a no-profit, no-loss healthcare facility started in March 2020. Despite the COVID-19 pandemic, it has continued to offer affordable, quality services supported by community donations. Spread across 1800 sq. ft., the center includes a pathology lab, digital X-ray, eye check-up unit, expert consultation services, medical equipment rental, and computerized consulting rooms. It provides home sample collection, has a trained staff, and offers expert consultation services across multiple specialties, while advanced scans are outsourced. The facility is air-conditioned, well-ventilated, and equipped to handle patient needs comfortably. It stands as a trusted health center focused on service, not profit."}
                         marathiText={"महावीर आय फाउंडेशनने छत्रपती संभाजीनगर येथे चालवलेले महावीर डायग्नोस्टिक सेंटर ही मार्च २०२० मध्ये सुरू केलेली एक ना-नफा, ना-तोटा आरोग्य सेवा सुविधा आहे. कोविड-१९ साथीच्या आजारानंतरही, ते समुदायाच्या देणग्यांद्वारे परवडणाऱ्या, दर्जेदार सेवा देत राहिले आहे. १८०० चौरस फूट क्षेत्रात पसरलेल्या या केंद्रात पॅथॉलॉजी लॅब, डिजिटल एक्स-रे, नेत्र तपासणी युनिट, तज्ञ सल्ला सेवा, वैद्यकीय उपकरणे भाड्याने देणे आणि संगणकीकृत सल्लागार कक्ष आहेत. ते घरी नमुना संकलन प्रदान करते, प्रशिक्षित कर्मचारी आहेत आणि अनेक विशेष सेवांमध्ये तज्ञ सल्ला सेवा देते, तर प्रगत स्कॅन आउटसोर्स केले जातात. ही सुविधा वातानुकूलित, हवेशीर आणि रुग्णांच्या गरजा आरामात हाताळण्यासाठी सुसज्ज आहे. ते नफ्यावर नव्हे तर सेवेवर लक्ष केंद्रित करणारे एक विश्वासार्ह आरोग्य केंद्र म्हणून उभे आहे."}
-                        fontSizeClass={fontStyles.fontSize20}
+                        fontSizeClass={fontStyles.fontSize22}
                         className={(useLanguage().language == "mr" ? fontStyles.textMr300 : "")} />
                 </div>
                 <div className={containerStyles.aboutImg}>
